6d02102e09fa4a2bee4ad7b394c210aa370fd173
Curder <q.curder@gmail.com>
2020-11-16 10:54:27 +0800

    Setting up the form

--- app/Http/Livewire/Auth/Register.php
+++ app/Http/Livewire/Auth/Register.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
+/**
+ * Class Register
+ * @property string email
+ * @property string password
+ * @property string passwordConformation
+ *
+ * @package App\Http\Livewire\Auth
+ */
 class Register extends Component
 {
+    public $email = '';
+    public $password = '';
+
+    public function register()
+    {
+        User::create([
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.auth.register');
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,3 +1,17 @@
-<div>
-    Register
-</div>
+<form action="#" method="POST" wire:submit.prevent="register">
+    {{ $email }}
+    <div>
+        <label for="email">Email</label>
+        <input wire:model="email" type="text" id="email" name="email">
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input wire:model="password" type="text" id="password" name="password">
+    </div>
+    <div>
+        <label for="passwordConformation">Password Conformation</label>
+        <input wire:model="passwordConformation" type="text" id="passwordConformation" name="passwordConformation">
+    </div>
+
+    <input type="submit" value="Register">
+</form>
