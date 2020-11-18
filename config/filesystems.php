7ec6238eb17cf25bc3ccfd3405fdc9159b1ada8d
Curder <q.curder@gmail.com>
2020-11-18 13:46:01 +0800

    aws s3 storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,10 +56,16 @@ return [
         ],
 
         'avatars' => [
-            'driver' => 'local',
-            'root' => storage_path('app/avatars'),
-            'url' => env('APP_URL').'/avatars',
+            'driver' => 's3',
+//            'root' => storage_path('app/avatars'),
+//            'url' => env('APP_URL').'/avatars',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
             'visibility' => 'public',
+            // 'endpoint' => env('AWS_ENDPOINT'),
         ],
 
         's3' => [
