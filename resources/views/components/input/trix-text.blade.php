2b84d75384f7f550715123601b353508f784c967
Curder <q.curder@gmail.com>
2020-11-19 21:51:02 +0800

    typo fixed

--- resources/views/components/input/trix-text.blade.php
+++ resources/views/components/input/trix-text.blade.php
@@ -9,7 +9,7 @@
     <input id="x" type="hidden" value="{{ $initialValue }}">
     <trix-editor input="x" class="form-textarea block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"></trix-editor>
 </div>
-@push('scripts')
+@push('styles')
 <link href="https://cdn.bootcdn.net/ajax/libs/trix/1.3.0/trix.min.css" rel="stylesheet">
 @endpush
 @push('scripts')
