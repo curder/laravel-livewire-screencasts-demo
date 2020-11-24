cfb71386a046c226269e4b4e5b25f406b0596ddc
curder <curder@foxmail.com>
2020-11-24 22:43:59 +0800

    with pagination

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -4,12 +4,15 @@ namespace App\Http\Livewire;
 
 use App\Models\Transaction;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Dashboard extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        $transactions = Transaction::paginate();
+        $transactions = Transaction::paginate(10);
 
         return view('livewire.dashboard', compact('transactions'));
     }
